749ee2e0ce319116ad4aff6ecead1495f9f61755
kensidiangco <kensidiangco@gmail.com>
2022-01-14 04:23:04 +0800

    Modifying files

---
+++ src/pages/HomePage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import useAxios from '../utils/useAxios'
+
+
+const HomePage = () => {
+    let [notes, setNotes] = useState([])
+
+    let api = useAxios()
+
+    useEffect(() => {
+        getNotes()
+    }, [notes])
+
+    let getNotes = async() => {
+        let response = await api.get('/api/notes/')
+        
+        if (response.status === 200) {
+            setNotes(response.data)
+        }
+    }
+
+    return (
+        <div>
+            <p>You are logged to the home page!</p>
+
+            <ul>
+                {notes.map(note => (
+                    <li key={note.id}>{note.body}</li>
+                ))}
+            </ul>
+        </div>
+
+    )
+}
+
+export default HomePage
